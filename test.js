f88c1832a45e63a2403be0dbbce5f92eb703ccc7
Donald Page <don.page.work@gmail.com>
2017-05-30 16:37:56 -0400

    test writen for user config and taking env variables for browser, server and project.

--- test.js
+++ test.js
@@ -1,9 +1,29 @@
 import test from 'ava';
 import assemble from '.';
 
-test('_buildConfig', t => {
-	const a = assemble(true);
-	console.log(`a.buildConfig`);
-	console.log(a.buildConfig);
-	t.true(true);
+test('buildConfig will take env variables for browser, server, and project.', t => {
+	const customConfig = {
+		server: 'browserstack.com',
+		browser: 'Chrome',
+		project: 'ENV-CONFIG'
+	};
+	process.env.SERVER = customConfig.server;
+	process.env.BROWSER = customConfig.browser;
+	process.env.PROJECT = customConfig.project;
+	const buildConfig = assemble(true).buildConfig;
+	t.true(buildConfig.browser === customConfig.browser);
+	t.true(buildConfig.server === customConfig.server);
+	t.true(buildConfig.project === customConfig.project);
+});
+
+test('buildConfig will take user config for browser, server, and project.', t => {
+	const customConfig = {
+		server: '172.0.0.1:4444',
+		browser: 'Safari',
+		project: 'USER-CONFIG'
+	};
+	const buildConfig = assemble(true, customConfig).buildConfig;
+	t.true(buildConfig.server === customConfig.server);
+	t.true(buildConfig.browser === customConfig.browser);
+	t.true(buildConfig.project === customConfig.project);
 });
